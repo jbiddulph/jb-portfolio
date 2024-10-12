46012392154ac93b3ad543dec7564e79d2e1b007
jbiddulph <john.mbiddulph@gmail.com>
2024-10-12 12:07:08 +0100

    made test changes and added naked sloth

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="container mx-auto flex mt-6 md:flex-row flex-col p-6 md:p-0">
-    <div class="w-100 md:w-2/3">
-      <div class="text-6xl md:text-8xl font-bold">I'm John,<br /> I've got new trainers, sunglasses, a shopping bag...<br /><span class="text-4xl md:text-6xl"> and I wear a hat!</span></div>
+    <div class="w-100 md:w-2/3 pr-6">
+      <div class="text-5xl md:text-5xl"><p>John Biddulph a passionate web developer specialising in modern JavaScript frameworks like Vue.js and Nuxt.</p> <p class="mt-6">Enjoys building intuitive, responsive applications that provide seamless user experiences.</p></div>
     </div>
     <div class="w-100 md:w-1/3 mt-6 md:mt-0">
       Projects I've built or worked on:
       <ul class="mt-6">
         <li class="mb-6">
           <h4 class="font-bold underline"><NuxtLink :href="'https://melvbiddulph.art'" target="_blank">‹Melvyn Biddulph Art /›</NuxtLink></h4>
-          <p>My dad was an artist, a good one but not that well known until his later years when he finaly started to exhibit.</p>
+          <p>Family first, my dad was an artist, a good one but not that well known until his later years.</p>
+        </li>
+        <li class="mb-6">
+          <h4 class="font-bold underline"><NuxtLink :href="'https://nakedsloth.co.uk'" target="_blank">‹NakedSloth - Rapr /›</NuxtLink></h4>
+          <p><span class="font-bold underline">Why?:</span> I wanted to build something using the OpenAI API and this is what I came up with, allowing people to create a design too and save it.</p>
         </li>
         <li class="mb-6">
           <h4 class="font-bold underline"><NuxtLink :href="'https://pinspots.co.uk'" target="_blank">‹Pin Spots /›</NuxtLink></h4>
@@ -16,7 +20,7 @@
         </li>
         <li class="mb-6">
           <h4 class="font-bold underline"><NuxtLink :href="'https://lookwhatfound.me'" target="_blank">‹LookWhatFound.Me /›</NuxtLink></h4>
-          <p><span class="font-bold underline">Why?:</span> This is about version 6 now, it was originally MyPubSpace, PubMic, PubHub, SplendidSussex, BN-Here. It's evolved from PHP, Laravel/Vue, Python/Nuxt, Nuxt/Supabase/Prisma</p>
+          <p><span class="font-bold underline">Why?:</span> A large database of pubs and venues accross the UK. This is about version 6 now, it was originally MyPubSpace, PubMic, PubHub, SplendidSussex, BN-Here. It's evolved from PHP, Laravel/Vue, Python/Nuxt, Nuxt/Supabase/Prisma</p>
         </li>
       </ul>
     </div>
